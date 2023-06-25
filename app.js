c7a0b4d3ceba7c8041c8299d6aaa3e5ddcb55488
SHIMADU YUKI <aileedelweiss@yahoo.co.jp>
2023-06-26 01:43:37 +0900

    ログインフォームを実装

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+document.querySelector(".email").value = localStorage.email || "";
+document.querySelector(".pass").value = localStorage.password || "";
+
+document.querySelector(".button").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  localStorage.email = document.querySelector(".email").value;
+  localStorage.password = document.querySelector(".pass").value;
+
+  this.closest("form").submit();
+});
